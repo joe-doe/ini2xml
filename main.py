51624fcfed2abe6b41fa8b1f2f7229a3d8d6dc12
Evan <e.rokkos@eunice-group.com>
2019-11-10 21:25:21 +0200

    Working prototype

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ from configparser import ConfigParser
 import xml.etree.cElementTree as ET
 
 
+def add_track(data, vlc_id, root_element):
+    track_element = ET.SubElement(root_element[1], "track")
+    ET.SubElement(track_element, "location").text = f'dvb-t://frequency={data["frequency"]}:bandwidth=8'
+    ET.SubElement(track_element, "title").text = f'{data["service_name"]}'
+    extension = ET.SubElement(track_element, "extension", attrib={'application': 'http://www.videolan.org/vlc/playlist/0'})
+    ET.SubElement(extension, "vlc:id").text = f'{vlc_id}'
+    ET.SubElement(extension, "vlc:option").text = 'dvb_adapter=0'
+    ET.SubElement(extension, "vlc:option").text = 'live-caching=300'
+    ET.SubElement(extension, "vlc:option").text = 'sout=#display'
+    ET.SubElement(extension, "vlc:option").text = 'no-sout-all'
+    ET.SubElement(extension, "vlc:option").text = 'sout-keep'
+    ET.SubElement(extension, "vlc:option").text = f'program={data["service_id"]}'
+    formatted_tree = ET.ElementTree(root)
+    formatted_tree.write("filename.xml", xml_declaration=True, encoding="UTF-8")
+
+
 config = ConfigParser()
 config.read('channels_1.conf')
 info = {}
@@ -10,30 +26,18 @@ for section in config.sections():
     info[section] = {k: v for k, v in config.items(section)}
 
 df = pd.DataFrame(info)
-print(df)
+# print(df)
 
 ET.register_namespace('', 'http://xspf.org/ns/0/')
 ET.register_namespace('vlc', 'http://www.videolan.org/vlc/playlist/ns/0/')
 
 tree = ET.parse('mine.xspf')
 root = tree.getroot()
-tracklist = root.find("trackList")
-for track in root[1]:
-    print(track.tag)
-    for track_attr in track.iter():
-        print(track_attr, track_attr.text)
+# tracklist = root.find("trackList")
+# for track in root[1]:
+#     print(track.tag)
+#     for track_attr in track.iter():
+#         print(track_attr, track_attr.text)
 
-track = ET.SubElement(root[1], "track")
-location = ET.SubElement(track, "location").text = 'AAA'
-title = ET.SubElement(track, "title").text = 'BBB'
-extension = ET.SubElement(track, "extension", attrib={'application': 'http://www.videolan.org/vlc/playlist/0'})
-id = ET.SubElement(extension, "vlc:id").text = '9999'
-dvb_adapter = ET.SubElement(extension, "vlc:option").text = 'dvb_adapter=0'
-live_caching = ET.SubElement(extension, "vlc:option").text = 'live-caching=300'
-sout = ET.SubElement(extension, "vlc:option").text = 'sout=#display'
-no_sout_all = ET.SubElement(extension, "vlc:option").text = 'no-sout-all'
-sout_keep = ET.SubElement(extension, "vlc:option").text = 'sout-keep'
-program = ET.SubElement(extension, "vlc:option").text = 'program=3066'
-
-root = ET.tostring(root)
-tree.write("filename.xml")
+for idx, column in enumerate(df):
+    add_track(df[column], idx, root)
